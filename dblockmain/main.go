0b4b9ae06608801c4a7f199de3830d95dd494d0f
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2016-01-30 23:02:49 +0100

    Fix indentation

--- dblockmain/main.go
+++ dblockmain/main.go
@@ -1,60 +1,60 @@
 package dblockmain
 
 import (
-	"github.com/hpcloud/tail"
-	"log"
-	"sync"
+  "github.com/hpcloud/tail"
+  "log"
+  "sync"
   "time"
   "github.com/timbuchwaldt/dblock/blockstore"
   "github.com/timbuchwaldt/dblock/incidentstore"
-	"github.com/timbuchwaldt/dblock/blocker"
+  "github.com/timbuchwaldt/dblock/blocker"
 )
 
 
 
 func Main() {
-	// read Config
+  // read Config
 
-	// compile regex
-	// start sync
-	// start storage
-	// start controller
-	// start follower + analyzer
+  // compile regex
+  // start sync
+  // start storage
+  // start controller
+  // start follower + analyzer
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+  var wg sync.WaitGroup
+  wg.Add(1)
 
-	log.Println("Hello world")
-	incidentChan := make(chan incidentstore.Incident, 100)
+  log.Println("Hello world")
+  incidentChan := make(chan incidentstore.Incident, 100)
   blockChan := make(chan blockstore.Block, 100)
-	blockControlChan := make(chan blocker.ControlMsg, 100)
+  blockControlChan := make(chan blocker.ControlMsg, 100)
 
-	go blocker.Blocker(blockControlChan)
+  go blocker.Blocker(blockControlChan)
   go blockstore.BlockStore(blockChan, blockControlChan)
   go incidentstore.IncidentStore(incidentChan, blockChan)
-	go follow_and_analyze("foo", incidentChan)
-	go follow_and_analyze("bar", incidentChan)
+  go follow_and_analyze("foo", incidentChan)
+  go follow_and_analyze("bar", incidentChan)
 
 
-	wg.Wait()
+  wg.Wait()
 
 }
 
 func follow_and_analyze(filename string, c chan incidentstore.Incident) {
-	t, err := tail.TailFile(filename,
-		tail.Config{
-			Follow:   true,
-			ReOpen:   true,
-			Location: &tail.SeekInfo{Offset: 0, Whence: 2}})
+  t, err := tail.TailFile(filename,
+    tail.Config{
+      Follow:   true,
+      ReOpen:   true,
+      Location: &tail.SeekInfo{Offset: 0, Whence: 2}})
 
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	for line := range t.Lines {
+  for line := range t.Lines {
 
-		// match against regexes here, if one matches, create "incident" struct
-		c <- incidentstore.Incident{Filename: filename, Ip: "192.168.100.1", Time: time.Now(), Line: line.Text}
-	}
+    // match against regexes here, if one matches, create "incident" struct
+    c <- incidentstore.Incident{Filename: filename, Ip: "192.168.100.1", Time: time.Now(), Line: line.Text}
+  }
 
 }
