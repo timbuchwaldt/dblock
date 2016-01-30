2807221316fa6a5f37a15e97de38514f8cef19bb
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2016-01-30 23:11:55 +0100

    Clean up, add prometheus exporter

--- dblockmain/main.go
+++ dblockmain/main.go
@@ -8,18 +8,19 @@ import (
   "github.com/timbuchwaldt/dblock/blockstore"
   "github.com/timbuchwaldt/dblock/incidentstore"
   "github.com/timbuchwaldt/dblock/blocker"
+  "flag"
+  "net/http"
+  "github.com/prometheus/client_golang/prometheus"
 )
 
+var addr = flag.String("listen-address", ":8080", "The address to listen for prometheus requests.")
 
 
 func Main() {
-  // read Config
+  flag.Parse()
+  http.Handle("/metrics", prometheus.Handler())
+  go http.ListenAndServe(*addr, nil)
 
-  // compile regex
-  // start sync
-  // start storage
-  // start controller
-  // start follower + analyzer
 
   var wg sync.WaitGroup
   wg.Add(1)
@@ -40,6 +41,17 @@ func Main() {
 
 }
 
+var (
+    incidentsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+      Name: "incidents_total",
+      Help: "Number of total dblock incidents",
+      })
+)
+
+func init(){
+  prometheus.MustRegister(incidentsCounter);
+}
+
 func follow_and_analyze(filename string, c chan incidentstore.Incident) {
   t, err := tail.TailFile(filename,
     tail.Config{
@@ -52,7 +64,7 @@ func follow_and_analyze(filename string, c chan incidentstore.Incident) {
   }
 
   for line := range t.Lines {
-
+    incidentsCounter.Inc()
     // match against regexes here, if one matches, create "incident" struct
     c <- incidentstore.Incident{Filename: filename, Ip: "192.168.100.1", Time: time.Now(), Line: line.Text}
   }
