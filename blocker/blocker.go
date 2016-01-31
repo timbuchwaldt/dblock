f80984691b05da445a3374fd3e54e931d77325f6
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2016-01-31 12:52:00 +0100

    Implement first bit of whitelisting

--- blocker/blocker.go
+++ blocker/blocker.go
@@ -11,29 +11,37 @@ type ControlMsg struct {
 }
 
 func Blocker(controlChan chan ControlMsg) {
+
+	var whitelist [2]net.IPNet
+	_, local, _ := net.ParseCIDR("127.0.0.1/8")
+	whitelist[0] = *local
 	log.Print("ipset create dblock hash:ip maxelem 1048576 -exist")
 	log.Print("ipset create dblock6 hash:ip maxelem 1048576 inet6 -exist")
 
 	for msg := range controlChan {
-		if msg.Block {
-			if msg.Ip.To4() != nil {
-				// this is an ipv4 address
-				log.Println("Blocking ip " + msg.Ip.String())
-				log.Print("ipset add dblock " + msg.Ip.String() + " -exist")
-			} else {
-				// this is an ipv6 address
-				log.Println("Blocking ip " + msg.Ip.String())
-				log.Print("ipset add dblock6 " + msg.Ip.String() + " -exist")
-			}
+		if local.Contains(msg.Ip) {
+			log.Println("IP is part of whitelisted networks")
 		} else {
-			if msg.Ip.To4() != nil {
-				// this is an ipv4 address
-				log.Println("Blocking ip " + msg.Ip.String())
-				log.Print("ipset del dblock " + msg.Ip.String() + " -exist")
+			if msg.Block {
+				if msg.Ip.To4() != nil {
+					// this is an ipv4 address
+					log.Println("Blocking ip " + msg.Ip.String())
+					log.Print("ipset add dblock " + msg.Ip.String() + " -exist")
+				} else {
+					// this is an ipv6 address
+					log.Println("Blocking ip " + msg.Ip.String())
+					log.Print("ipset add dblock6 " + msg.Ip.String() + " -exist")
+				}
 			} else {
-				// this is an ipv6 address
-				log.Println("Blocking ip " + msg.Ip.String())
-				log.Print("ipset del dblock6 " + msg.Ip.String() + " -exist")
+				if msg.Ip.To4() != nil {
+					// this is an ipv4 address
+					log.Println("Blocking ip " + msg.Ip.String())
+					log.Print("ipset del dblock " + msg.Ip.String() + " -exist")
+				} else {
+					// this is an ipv6 address
+					log.Println("Blocking ip " + msg.Ip.String())
+					log.Print("ipset del dblock6 " + msg.Ip.String() + " -exist")
+				}
 			}
 		}
 	}
